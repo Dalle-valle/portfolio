6f3a729d059982cafdf75a80f12cd42defd2560c
Daniel Dalby <danieldalby@Daniels-MacBook-Pro.local>
2020-01-02 19:24:21 +0100

    Burgermenuen er færdig med animation

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 window.addEventListener("load", sidenVises);
 
+
+// Når siden loades tilføjes der 2 eventlisteners til menuen. En til touch og en til klik som kalder funktionen der åbner og lukker menuen.
+
 function sidenVises() {
     console.log("siden vises");
     document.querySelector(".container").addEventListener("touched", menuOpenClose);
     document.querySelector(".container").addEventListener("click", menuOpenClose);
 
 }
+
+// Funktionen der åbner og lukker menuen. Har kaldt #mylinks for x for at simplificere. Når menuen er i display block, skal den sættes til display none, og hvis den er i none skal den display block. Dette sørger for at den altid gør den modsatte end hvad den er.
 
 function menuOpenClose() {
     console.log("menu pressed");
